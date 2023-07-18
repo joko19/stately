35de0aee988fca3cee344323591c7d9855248cab
joko19 <jokoslamet2207@gmail.com>
2023-07-18 16:43:13 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      {/* <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <section className="mt-12">
+          <h1 className="font-bold text-2xl" >Menyederhanakan Pencatatan Keuangan dari Laporan Bank Kamu.</h1>
+          <p>
+            Mencatat pengeluaran dan pemasukan merupakan hal yang memakan waktu,
+            apalagi dengan laporan bank yang perlu banyak disortir, dianalisa,
+            dan diketik. Tenang, kami akan membantu anda memudahkan proses ini!
+            Hanya dengan mengunggah laporan bank (estatement) kamu dalam format
+            PDF, kamu akan dengan mudah mendapatkan laporan kamu dengan bentuk
+            EXCEL.
+          </p>
+        </section>
+      </main> */}
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-primary">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex gap-2 items-center text-white">
+              <img src="/asset/icon/Scan.svg" alt="logo" />
+              <h1 className="text-2xl">STATELY</h1> - <span className="font-medium" >Bank Statement Converter</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
